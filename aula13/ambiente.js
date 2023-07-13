d0ce559a245e746b1f31f145d9250b92bf197c82
Ana-Sato <ana.freire@edu.unipar.br>
2023-07-13 14:49:49 -0300

    novos exercicios

---
+++ aula13/ambiente.js
@@ -0,0 +1,41 @@
+/* Estruturas de repeticao:
+while = Enquanto 
+Esrutrura mais simples com teste logico no inicio */
+
+var c = 1
+while (c <= 6) {
+    console.log('Tudo bem?')
+    c++
+}
+
+var c = 1
+while (c <= 3) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+
+ 
+/* Estrutura DO WHILE:
+   estrutura de repeticao com teste logico no final
+*/
+
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
---
+++ aula14/ambiente.js
@@ -0,0 +1,12 @@
+/* Estruturas de repeticao:
+for = repetições com variáveis de controle */
+/*
+for(var c = 1; c <= 10; c ++) {
+    console.log(c)
+}
+*/
+console.log('Vai comecar...')
+for(var c = 1; c <= 4; c++) {
+    console.log(c)
+}
+console.log('Fim!!!')
