8905786e9606338ed3c5942f69383a136c239ec4
Ana-Sato <ana.freire@edu.unipar.br>
2023-07-13 15:25:04 -0300

    exercicios criados

--- aula14-ex/ex016/script.js
+++ aula14-ex/ex016/script.js
@@ -1,1 +1,30 @@
-alert('Ola!!!')
+function contar() {
+    let ini = document.getElementById('txt1') 
+    let fim = document.getElementById('txt2')
+    let passo = document.getElementById('txtp')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length ==0) {
+        res.innerHTML = 'Impossivel contar!'
+        //window.alert('[ERRO] Faltam dados!')
+    } else {
+       res.innerHTML = 'Contando: <br>'
+       let i = Number(ini.value)
+       let f = Number(fim.value)
+       let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo invalido! Considerando PASSO 1')
+            p = 1
+        }
+       if (i < f) {
+            for (let c = i; c <= f; c+= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+            res.innerHTML += `\u{1F3C1}`
+       } else {
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+            res.innerHTML += `\u{1F3C1}`
+       }
+    }
+}
