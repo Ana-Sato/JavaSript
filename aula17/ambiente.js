dadaaf1144a99a07f7fd721e5ea1eaa475514108
Ana-Sato <ana.freire@edu.unipar.br>
2023-07-14 15:21:09 -0300

    aula17

---
+++ aula17/ambiente.js
@@ -0,0 +1,13 @@
+/* Objetos:
+Sao variaveis(guardam valores) que alem de guardar valores guardam funcionalidades(metodos)
+*/
+let amigo = {nome: 'Jose', 
+sexo: 'M',
+peso: 85.4,
+engordar(p=0){
+    console.log('Engordou')
+    this.peso += p
+}}
+
+amigo.engordar(2)
+console.log(`${amigo.nome} pesa ${amigo.peso}Kg.`)
